ab178bfafcb9c8cf353bca723bdc2829e0f39b9b
Zoe Ames <zoeames@gmail.com>
2015-06-03 20:20:51 -0500

    authentication finished

--- public/js/controllers/authcontroller.js
+++ public/js/controllers/authcontroller.js
@@ -1,7 +1,8 @@
-controllers.controller('AuthCtrl', function ($scope) {
+controllers.controller('AuthCtrl', function ($scope, $http, $rootScope, $location) {
   $scope.message='NEWAuthenticateCtrl'
   $scope.showRegister = true;
   $scope.showLogin = false;
+  $scope.user= {};
 
   $scope.showLoginBtn = function(){
     $scope.showRegister = false;
@@ -12,5 +13,41 @@ controllers.controller('AuthCtrl', function ($scope) {
     $scope.showRegister = true;
     $scope.showLogin = false;    
   }
+
+  $scope.register = function(){
+    console.log('you clicked the register button')
+    $http({
+          method: 'POST',
+          url: '/api/user/register',
+          data: $scope.user
+        }).
+        success(function (data, status, headers, config) {
+          console.log('you registered!')
+          $scope.user={};
+          $scope.showRegister = false;
+          $scope.showLogin = true;
+        }).
+        error(function (data, status, headers, config) {
+          console.log('register failed')
+        });
+  }
+
+$scope.login = function(){
+    console.log('you clicked the register button')
+    $http({
+          method: 'POST',
+          url: '/api/user/login',
+          data: $scope.user
+        }).
+        success(function (data, status, headers, config) {
+          console.log('you logged in!', data);
+          $rootScope.rootuser = data.username;
+          $location.path('/todo');
+        }).
+        error(function (data, status, headers, config) {
+          console.log('login failed')
+        });
+  }
+
 
 })
---
+++ public/js/controllers/navcontroller.js
@@ -0,0 +1,8 @@
+controllers.controller('NavCtrl', function ($scope, $rootScope, $location) {
+  $scope.message = "NAV!"
+  $scope.logout = function(){
+    $rootScope.rootuser = null;
+    $location.path('/')
+  }
+
+});
